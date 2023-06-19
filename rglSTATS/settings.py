e4d37b4449ae5655eebbf5d8b7d5450ffcf42f24
Josh <jstanzeleit@agexchangegroup.com>
2023-06-19 15:29:12 -0600

    fixed static warning i think

--- rglSTATS/settings.py
+++ rglSTATS/settings.py
@@ -135,7 +135,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_ROOT = BASE_DIR
+STATIC_ROOT = 'static/'
 
 STATIC_URL = 'static/'
 
