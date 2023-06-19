415fef7696069123e78548502c9d5b0ca227031c
Josh <jstanzeleit@agexchangegroup.com>
2023-06-19 16:08:02 -0600

    fixed url issues

--- leaderboards/urls.py
+++ leaderboards/urls.py
@@ -4,12 +4,12 @@ from . import views
 
 urlpatterns = [
      # ex: /leaderboards/
-    path('', views.index, name='index'),
-    path('lookup/', views.lookup, name='lookup'),
+    path('leaderboards/', views.index, name='index'),
+    path('', views.lookup, name='lookup'),
     path('lookup/<int:player_id>/', views.lookup, name='lookup'),
     path('player/<int:player_id>/', views.stat, name='stat'),
-    path('<str:div>/', views.index, name='index'),
-    path('<str:div>/<str:c>/', views.index, name='index'),
+    path('leaderboards/<str:div>/', views.index, name='index'),
+    path('leaderboards/<str:div>/<str:c>/', views.index, name='index'),
     # ex: /leaderboards/5/
     
     # ex: /leaderboards/5/name/
--- rglSTATS/urls.py
+++ rglSTATS/urls.py
@@ -18,6 +18,6 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path('leaderboards/', include('leaderboards.urls')),
+    path('', include('leaderboards.urls')),
     path('admin/', admin.site.urls),
 ]
